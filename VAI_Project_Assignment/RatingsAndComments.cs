a25540f2aa917d673ac3050d0d811ffd79002245
Vygas Simonaitis <VS655@student.aru.ac.uk>
2023-12-02 01:08:43 +0000

    Basic UI for the Ratings and Comments section, including a header, text box, rating from one to five stars and a submit button.

---
+++ VAI_Project_Assignment/RatingsAndComments.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VAI_Project_Assignment
+{
+    public partial class RatingsAndComments : Form
+    {
+        public RatingsAndComments()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
